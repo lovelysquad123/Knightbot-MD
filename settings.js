3d86ecb01a386d31797f7069960934dae0e84c96
lovelysquad123 <avstamilzan@gmail.com>
2025-04-20 10:32:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Lovely', // Your name
-  ownerNumber: '94764642432', //Your number
+  ownerNumber: '+94764642432', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
